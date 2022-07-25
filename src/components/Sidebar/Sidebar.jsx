437c2c2af5cb4bca12c8c098e8471a2f747cdaa4
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-07-25 16:47:44 +0300

    burger icon logic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -20,11 +20,11 @@ const Sidebar = () => {
     const toggleMin = () => setMin(!min); 
     return (
         <aside className={styles.sidebar} role='complementary'>
-            <div className={min ? `${styles.sidebar__container} ${styles.sidebar__container_min}` : styles.sidebar__container}>
+            <div className={min ? `${styles.sidebar__container} ${styles.sidebar__container_min} _container` : `${styles.sidebar__container} _container`}>
                 <div className={styles.sidebar__top}>
                     <h1 className={min ? styles.sidebar__logo : `${styles.sidebar__logo} ${styles.sidebar__logo_active}`}>Logo</h1>
                     <div onClick={toggleMin} className={min ? `${styles.sidebar__bars} ${styles.sidebar__bars_min}` : styles.sidebar__bars}>
-                        <div  className={styles['sidebar__bars-item']}></div>
+                        <div className={styles['sidebar__bars-item']}></div>
                     </div>
                 </div>
                 <div className={styles.sidebar__menu}>
