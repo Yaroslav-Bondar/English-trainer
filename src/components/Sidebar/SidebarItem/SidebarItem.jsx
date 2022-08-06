10e3786a6dbf91eda85d690b85fec1ae5968f433
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-08-06 04:54:59 +0300

    sidebar component logic

--- src/components/Sidebar/SidebarItem/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem/SidebarItem.jsx
@@ -1,34 +1,29 @@
 import {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { FaBeer } from 'react-icons/fa';
-import styles from './SidebarItem.module.css';
+import styles from './SidebarItem.module.scss';
 
 const SidebarItem = ({item, min, toggleMin}) => {
     const [open, setOpen] = useState(false);
-    // console.log('min setItem ', min);
-    console.log('open && !min ', open && !min);
-
+    const fullOpen = open && !min;
     if(item.childrens) {
         return (
-            <div className={open && !min ? `${styles.sidebar__item} ${styles.sidebar__item_open}` : styles.sidebar__item}>
-                <div onClick={() => setOpen(!open)} className={styles.sidebar__title}>
+            <div className={fullOpen ? `${styles.sidebar__item} ${styles.sidebar__item_open}` : styles.sidebar__item}>
+                <div 
+                    onClick={() => {
+                        if(min) {toggleMin(!min); setOpen(true)} else {setOpen(!open)}   console.log('sidebar__title click')
+                    }} 
+                    className={styles.sidebar__title}
+                >
                     <div className={styles['sidebar__title-container']}>
-                        <div 
-                            className={`${styles['sidebar__item-title-icon']} folder`}
-                            onClick={() => {if(min) {toggleMin(!min); setOpen(true)} }}
-                        >
-                            <div className={open ? "folder__container folder__container_open" : "folder__container"}>
+                        <div className={`${styles['sidebar__title-icon']} folder`}>
+                            <div className={fullOpen ? "folder__container folder__container_open" : "folder__container"}>
                                 <div className="folder__backside"></div>
                                 <div className="folder__file-container">
                                     <div className="folder__file"></div>
-                                    <div className="folder__file"></div>
-                                    <div className="folder__file"></div>
-                                    <div className="folder__file"></div>
-                                    <div className="folder__file"></div>
                                 </div>
                                 <div className="folder__frontside">
-                                    {item.icon && <div class="folder__icon bi-github"></div>}
+                                    {item.icon && <div class={`folder__icon ${item.icon}`}></div>}
                                 </div>
                             </div>
                         </div>
@@ -47,9 +42,9 @@ const SidebarItem = ({item, min, toggleMin}) => {
     } else {
         return (
             <NavLink to={item.path || '#'} className={`${styles.sidebar__item} ${styles.sidebar__item_plain}`}>
-                {item.icon && <i className={styles['sidebar__item-plain-icon']}>icon</i>}
+                {item.icon && <i className={`${styles['sidebar__plain-icon']} ${item.icon}`}></i>}
                 <span 
-                    className={!min ? `${styles['sidebar__item-title-plain']} ${styles['sidebar__item-title-plain_active']}` : styles['sidebar__item-title-plain']}
+                    className={!min ? `${styles['sidebar__plain-title']} ${styles['sidebar__plain-title_active']}` : styles['sidebar__plain-title']}
                 >
                     {item.title}
                 </span>
